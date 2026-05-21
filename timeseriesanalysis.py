593396ac833bb8f4f2b6df139c9f7d035f4fee85
shashwat mukesh bhajanka <shashwatbhajanka@Shashwatbhajanka.local>
2026-05-21 17:37:36 +0530

    Add boxplot and lineplot functions for unemployment analysis

--- timeseriesanalysis.py
+++ timeseriesanalysis.py
@@ -3,5 +3,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import seaborn as sb
+# pyrefly: ignore [missing-import]
+from ydata_profiling import ProfileReport
 
-print("hello")
+df = pd.read_csv("employment.csv")
+
+def AgeWiseBox():
+    plt.figure(figsize = (15,8))
+    ax = sb.boxplot(data = df)
+    ax.set_title("Age wise unemployment")
+    plt.xticks(rotation  = 90)
+    ax.set_xlabel('Industry')
+    ax.set_ylabel('Unemployment Rate')
+    plt.show()
+
+def lineplot():
+    lc = df.plot(subplots = True, figsize = (10,20))
+    plt.xlabel("Year")
+    plt.ylabel("Unemployment Rate")
+    plt.show()
+    
+lineplot()
+    
