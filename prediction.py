8fc33006322ea09a7f0086b357c1e09da67fed69
ShashwatBhajanka <bhajankashashwat@gmail.com>
2026-05-28 16:15:08 +0530

    adding new prediction file with needed libraries

---
+++ prediction.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import statsmodels.api as sm
+import numpy as np
+import warnings
+import os
